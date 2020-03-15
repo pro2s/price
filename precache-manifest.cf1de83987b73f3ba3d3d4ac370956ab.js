6604b4c9211dd1edfa98f900213e4a27a0fb55e5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-15 08:48:47 +0000

    Deploy pro2s/price to github.com/pro2s/price.git:gh-pages

---
+++ precache-manifest.cf1de83987b73f3ba3d3d4ac370956ab.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ef70b5848d0c4f1869848fd0ee8396",
+    "url": "/price/index.html"
+  },
+  {
+    "revision": "0098391b1b86d9c28b13",
+    "url": "/price/static/css/main.0a44f458.chunk.css"
+  },
+  {
+    "revision": "b55e621a7c0c980d7274",
+    "url": "/price/static/js/2.c249c04a.chunk.js"
+  },
+  {
+    "revision": "0098391b1b86d9c28b13",
+    "url": "/price/static/js/main.edfeace1.chunk.js"
+  },
+  {
+    "revision": "6bb7744e4129bd46cc02",
+    "url": "/price/static/js/runtime~main.348d043b.js"
+  },
+  {
+    "revision": "6c426583a257819912278cedfdd9badf",
+    "url": "/price/static/media/item1.6c426583.jpg"
+  },
+  {
+    "revision": "8779dd15782203466bfb67cac99ef2ec",
+    "url": "/price/static/media/item2.8779dd15.jpg"
+  },
+  {
+    "revision": "9bd2546f523e4a6e2515068c65ca441c",
+    "url": "/price/static/media/item3.9bd2546f.jpg"
+  },
+  {
+    "revision": "65bc33d70f7f4ca793c5acc24525de9f",
+    "url": "/price/static/media/item4.65bc33d7.jpg"
+  },
+  {
+    "revision": "f5fd97b56137c3ed5df0f3f71482c105",
+    "url": "/price/static/media/item6.f5fd97b5.jpg"
+  },
+  {
+    "revision": "ad50860783f73f95f9ef6fb0a57eeb96",
+    "url": "/price/static/media/shoeinsoles-1.ad508607.jpg"
+  },
+  {
+    "revision": "1128701aa6541d23dda474d1575d336c",
+    "url": "/price/static/media/shoeinsoles-2.1128701a.jpg"
+  },
+  {
+    "revision": "f93942d3c185f0a2530edaf40d0f4d38",
+    "url": "/price/static/media/shoeinsoles-3.f93942d3.jpg"
+  },
+  {
+    "revision": "e44b21ec0f672c77c9cd7755007bf38f",
+    "url": "/price/static/media/shoelaces-1.e44b21ec.jpg"
+  },
+  {
+    "revision": "506fdc45a7e340a87b5e83dd8e6605ba",
+    "url": "/price/static/media/shoelaces-2.506fdc45.jpg"
+  },
+  {
+    "revision": "ecaa975ab2cc10da5d1692fb27f1a4f0",
+    "url": "/price/static/media/shoelaces-3.ecaa975a.jpg"
+  }
+]);
